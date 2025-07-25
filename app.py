0363631f6091e17fd8c3605fe2869c674d67f1d8
rohaan098 <51942524+rohaan098@users.noreply.github.com>
2025-07-25 13:14:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,5 @@ def index():
     return render_template('index.html', key_phrases=key_phrases, restaurant=restaurant_name, reviews=reviews)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render sets PORT automatically
+    app.run(host='0.0.0.0', port=port)
